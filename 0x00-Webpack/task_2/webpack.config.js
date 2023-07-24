7c488dfa6e9a7cc641d2ec12ae354a6bdcd8e5be
Yoseph <ymymym4634@gmail.com>
2023-07-24 21:54:11 +0300

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -19,32 +19,13 @@ module.exports = {
 
             {
                 test: /\.(?:png|svg|jpg|jpeg|gif)$/i,
-                loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-            },
-            },
-            {
-                loader: 'image-webpack-loader',
-                options: {
-                  mozjpeg: {
-                    progressive: true,
-                  },
-                  optipng: {
-                    enabled: false,
-                  },
-                  pngquant: {
-                    quality: [0.65, 0.9],
-                    speed: 4,
-                  },
-                  gifsicle: {
-                    interlaced: false,
-                  },
-                  webp: {
-                    quality: 75,
-                  },
-                },
+                type: 'asset/resource',
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                    },
+                ],
             },
         ],
     },  
