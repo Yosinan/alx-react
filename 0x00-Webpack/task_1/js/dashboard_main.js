12ebabcef19be1a6032f1727802889a9633a963e
Yoseph <ymymym4634@gmail.com>
2023-07-24 18:18:11 +0300

    Webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,11 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $("body").append("<p>Copyright - Holberton School</p>");
 
-let counter = 0;
+let count = 0;
 
 function updateCounter(){
-	counter += 1;
-	$("#count").html(`${counter} clicks on the button`);
+	count += 1;
+	$("#count").html(`${count} clicks on the button`);
 }
 
 $('button').on('click', _.debounce(updateCounter, 500));
