ae2246b7e222b007c61361e912d8e04c52fa9c6b
Yoseph <ymymym4634@gmail.com>
2023-07-24 20:41:38 +0300

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,62 +1,35 @@
-// const path = require('path');
-
-// module.exports = {
-//     mode: 'production',
-//     entry: {
-//         main: path.resolve(__dirname, 'js', 'dashboard_main.js'),
-//     },
-//     output: {
-//         path: path.resolve(__dirname, 'public'),
-//         filename: 'bundle.js',
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.css$/i,
-//                 use: [ 'style-loader', 'css-loader' ],
-//             },
-
-//             {
-//                 test: /\.(?:png|svg|jpg|jpeg|gif)$/i,
-//                 type: 'asset/resource',
-
-//             },
-//         ],
-//     },  
-// };
-
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  	mode: 'production',
-	entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public'
-  },
-  module: {
-	rules: [
-		{
-			test: /\.css$/i,
-			use: ["css-loader", "style-loader"],
-		},
-		{
-			test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
-			type: 'asset/resource',
-			use: [
-				"file-loader",
-				{
-					loader: "image-webpack-loader",
-					options: {
-							bypassingOnDebug: true,
-							disable: true,
-					},
-				},
-			],
-		},
-	],
-},
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, 'js', 'dashboard_main.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ 'style-loader', 'css-loader' ],
+            },
+
+            {
+                test: /\.(?:png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                                bypassingOnDebug: true,
+                                disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },  
 };
