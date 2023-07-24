70861ef551e991d55965c56f5dccacfcf95e7ddc
Yoseph <ymymym4634@gmail.com>
2023-07-24 20:56:29 +0300

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js',
+        publicPath: '/public',
     },
     module: {
         rules: [
