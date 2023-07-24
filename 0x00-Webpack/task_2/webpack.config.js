a9893f21efd7b884bbbd96a2e9454c8b3580d96c
Yoseph <ymymym4634@gmail.com>
2023-07-24 22:12:50 +0300

    Webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,35 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  	mode: 'production',
-	  entry: {
-      main: path.resolve(__dirname, './js/dashboard_main.js'),
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, 'js', 'dashboard_main.js'),
     },
     output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js',
-      publicPath: '/public'
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/public',
     },
     module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["css-loader", "style-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
-        type: 'asset/resource',
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-                bypassingOnDebug: true,
-                disable: true,
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [ 'style-loader', 'css-loader' ],
             },
-          },
+
+            {
+                test: /\.(?:ico|png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                          bypassOnDebug: true, // webpack@1.x
+                          disable: true, // webpack@2.x and newer
+                        },
+                    }, 
+                ],
+            },
         ],
-      },
-    ],
-  },
+    },  
 };
