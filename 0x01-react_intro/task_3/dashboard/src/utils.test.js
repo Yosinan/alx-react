6d97940e6e4199337ed027830efd6af003cece7e
Yoseph <ymymym4634@gmail.com>
2023-07-26 13:44:31 +0300

    Creating React tests

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,26 +1,32 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
 describe("Get full year", function() {
+    // should return the correct year
   it("should return the correct year", function() {
     const currentYear = new Date().getFullYear();
     expect(getFullYear()).toBe(currentYear);
   });
 })
 
+// should return the correct footer message based on the argument
 describe("Get footer message", function() {
   const trueMessage = 'Holberton School';
   const falseMessage = 'Holberton School main dashboard';
 
+  // should return the true footer message
   it("should return the true footer message", function() {
     const currentMessage = getFooterCopy(true);
     expect(currentMessage).toBe(trueMessage);
   });
+
+    // should return the false footer message
   it("should return the false footer message", function() {
     const currentMessage = getFooterCopy(false);
     expect(currentMessage).toBe(falseMessage);
   })
 })
 
+// should return the correct string when calling getLatestNotification
 describe("Get latest notification", function() {
   it("should return the urgent message text", function() {
     const message = '<strong>Urgent requirement</strong> - complete by EOD';
