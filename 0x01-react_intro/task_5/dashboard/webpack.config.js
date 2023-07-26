fbc981700e1bb625cc40e87bca7107501b27b5d2
Yoseph <ymymym4634@gmail.com>
2023-07-26 22:07:16 +0300

    Create a project using Webpack

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     devtool: 'inline-source-map',
     plugins: [
-        new CleanWebpackPlugin(),
         new HTMLWebpackPlugin({
             title: 'Holberton Dashboard',
             filename: './index.html',
@@ -18,18 +17,10 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
-    optimization: {
-		splitChunks: {
-			chunks: 'all',
-		},
-	},
 	devServer: {
 		static: path.join(__dirname, './dist'),
 		open: true,
 		port: 8564,
-	},
-	performance: {
-		maxAssetSize: 1000000,
 	},
     module: {
         rules: [
