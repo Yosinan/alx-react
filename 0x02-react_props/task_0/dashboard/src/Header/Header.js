8dbde5a918283802695d1bcf2f405a192ae12ed6
Yoseph <ymymym4634@gmail.com>
2023-07-31 12:11:46 +0300

    React props

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,14 @@
+import logo from '../Holberton_logo.jpg';
+import './Header.css';
+
+
+export default function Header() {
+    return (
+        <>
+            <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className='title'>School dashboard</h1>
+            </div>
+        </>
+    )
+}
