5c2f361ba1d9aa0bf14f324f3ff364cbc065355c
Yoseph <ymymym4634@gmail.com>
2023-08-11 21:48:44 +0300

    React Inline Styling

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import '@testing-library/jest-dom/extend-expect';
-
 import CourseListRow from "./CourseListRow";
 
 describe("CourseListRow component", () => {
