9ad338cec18278189083952b736d6c1d35ee19ca
Yoseph <ymymym4634@gmail.com>
2023-08-13 15:06:04 +0300

    React Inline Styling

---
+++ 0x04-React_inline_styling/task_3/dashboard/src/App/App.js
@@ -0,0 +1,111 @@
+import React from "react";
+// import { useState } from "react";
+import PropTypes from "prop-types";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Notifications from "../Notifications/Notifications";
+import { getLatestNotification } from "../utils/utils";
+import CourseList from "../CourseList/CourseList";
+import { StyleSheet, css } from "aphrodite";
+import BodySection from "../BodySection/BodySection";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+
+export default class App extends React.Component {
+  listCourses = [
+    { id: 1, name: "ES6", credit: 60 },
+    { id: 2, name: "Webpack", credit: 20 },
+    { id: 3, name: "React", credit: 40 },
+  ];
+
+  listNotifications = [
+    { id: 1, value: "New course available", type: "default" },
+    { id: 2, value: "New resume available", type: "urgent" },
+    { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
+  ];
+
+  constructor(props) {
+    super(props);
+    this.isLoggedIn = props.isLoggedIn;
+    this.logOut = props.logOut;
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
+      this.logOut();
+    }
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="root-notifications">
+          <Notifications listNotifications={this.listNotifications} />
+        </div>
+        <div className="App">
+          <Header />
+          <div className={css(bodyStyle.App)}>
+            {this.props.isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={this.listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>
+            )}
+            <BodySection title="News from the School">
+              <p>Lorem ipsum ...</p>
+            </BodySection>
+          </div>
+          <div className={css(footerStyle.footerStyle)}>
+            <Footer />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const bodyStyle = StyleSheet.create({
+  App: {
+    marginLeft: "90px",
+    marginTop: "70px",
+    position: "relative",
+  },
+});
+
+const footerStyle = StyleSheet.create({
+  footerStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
+    borderTop: "3px solid #ce314b",
+    textAlign: "center",
+    fontStyle: "italic",
+    fontSize: "12px",
+  },
+});
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
