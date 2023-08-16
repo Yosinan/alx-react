d1b9538cae0efd86709d8d8e30180d5cfd1856ce
Yoseph <ymymym4634@gmail.com>
2023-08-16 23:03:01 +0300

    React_Immutable

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -3,12 +3,3 @@ const { Map } = require('immutable');
 export default function getImmutableObject (obj) {
   return Map(obj);
 }
-
-// const obj = {
-//   fear: true,
-//   smell: -1033575916.9145899,
-//   wall: false,
-//   thing: -914767132
-// };
-
-// console.log(getImmutableObject(obj));
