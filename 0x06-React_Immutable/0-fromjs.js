7c6aa9150bafc77002677262646a9d27ed4955e2
Yoseph <ymymym4634@gmail.com>
2023-08-16 22:02:12 +0300

    React_Immutable

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,16 @@
+const { fromJS } = require("immutable");
+
+function getImmutableObject(obj) {
+  return fromJS(obj);
+}
+
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+console.log(getImmutableObject(obj));
+
+module.exports = getImmutableObject;
