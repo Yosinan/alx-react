71ef55f1980b8934040743c5988a35ce1b763810
Yosinan <ymymym4634@gmail.com>
2023-08-18 16:34:09 +0000

    React Immutable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(obj) {
 	  return fromJS(obj);
 }
-=======
-import { fromJS } from 'immutable';
-
-export default function getImmutableObject(obj) {
-	  return fromJS(obj);
-}
->>>>>>> e43372c4a816f2cabeaa29bb184ad177e71a07e9
