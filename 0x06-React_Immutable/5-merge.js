0e8642fbc021a182b7014713ca1bbd976e5b622c
Yoseph <ymymym4634@gmail.com>
2023-08-20 22:10:39 +0300

    React Immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,6 +1,6 @@
 import { List, Map } from 'immutable';
 
-export default function concatElements(page1, page2) {
+export function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
