ce685d6e64b86ed5c4be42bcac2ec463c01ffbac
Yoseph <ymymym4634@gmail.com>
2023-08-21 18:45:42 +0300

    React State

---
+++ 0x05-react_state/task_2/dashboard/src/App/App.test.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+describe("App component", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders a div with the class root-notifications", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".root-notifications")).toHaveLength(1);
+  });
+
+  it("renders a div with the class App-header", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-header")).toHaveLength(1);
+  });
+
+  it("renders a div with the class App-body", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-body")).toHaveLength(1);
+  });
+
+  it("renders a div with the class App-footer", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(".App-footer")).toHaveLength(1);
+  });
+
+  it("should not display CourseList when isLoggedIn is false", () => {
+    const { queryByText } = render(<App />);
+    const courseListElement = queryByText(/Course List/i);
+    expect(courseListElement).toBeNull();
+  });
+
+  describe("when isLoggedIn is true", () => {
+    it("should not include the Login component", () => {
+      const { queryByText } = render(<App isLoggedIn={true} />);
+      const loginElement = queryByText(/Login/i);
+      expect(loginElement).toBeNull();
+    });
+
+    it("should include the CourseList component", () => {
+      const { queryByText } = render(<App isLoggedIn={true} />);
+      const courseListElement = queryByText(/Course List/i);
+      expect(courseListElement).toBeInTheDocument();
+    });
+  });
+});
+
+describe("Testing App Component's State />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<App />);
+  });
+
+  it("check if default value of displayDrawer in state is false", () => {
+    expect(wrapper.state("displayDrawer")).toBe(false);
+  });
+
+  it("Verify that after calling handleDisplayDrawer, the state displayDrawer should now be true", () => {
+    wrapper.instance().handleDisplayDrawer();
+    expect(wrapper.state("displayDrawer")).toBe(true);
+  });
+
+  it("verify that after calling handleHideDrawer, the state displayDrawer is updated to be false", () => {
+    wrapper.instance().handleHideDrawer();
+    expect(wrapper.state("displayDrawer")).toBe(false);
+  });
+});
+
+describe("Testing <App logOut={function} />", () => {
+  it("verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
+    const wrapper = mount(
+      <App
+        logOut={() => {
+          console.log("ctrl and h are pressed");
+        }}
+      />
+    );
+    window.alert = jest.fn();
+    const inst = wrapper.instance();
+    const logout = jest.spyOn(inst, "logOut");
+    const alert = jest.spyOn(window, "alert");
+    const event = new KeyboardEvent("keydown", {
+      bubbles: true,
+      ctrlKey: true,
+      key: "h",
+    });
+    document.dispatchEvent(event);
+    expect(alert).toBeCalledWith("Logging you out");
+    expect(logout).toBeCalled();
+    alert.mockRestore();
+  });
+});
---
+++ 0x05-react_state/task_2/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { getFooterCopy, getFullYear } from "../utils/utils";
+
+export default function Footer() {
+  return (
+    <>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+    </>
+  );
+}
