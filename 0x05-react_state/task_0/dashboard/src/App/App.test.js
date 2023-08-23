ecc44535ab51c933e349a61a9be83def4f86bfde
Yoseph <ymymym4634@gmail.com>
2023-08-23 12:04:41 +0300

    React State

--- 0x05-react_state/task_0/dashboard/src/App/App.test.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.test.js
@@ -1,53 +1,85 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import { jest } from "@jest/globals";
 import App from "./App";
 import { StyleSheetTestUtils } from "aphrodite";
 
-StyleSheetTestUtils.suppressStyleInjection();
+describe("Test App.js", () => {
+  let wrapper;
 
-describe("App component", () => {
-  it("renders without crashing", () => {
-    shallow(<App />);
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<App />);
   });
 
-  it("renders a div with the class root-notifications", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".root-notifications")).toHaveLength(1);
+  it("Renders App without crashing", () => {
+    expect(wrapper.exists());
   });
 
-  it("renders a div with the class App-header", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-header")).toHaveLength(1);
+  it("App component contains Notifications component", () => {
+    expect(wrapper.find("Notifications")).toHaveLength(1);
   });
 
-  it("renders a div with the class App-body", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-body")).toHaveLength(1);
+  it("App component contains Header component", () => {
+    expect(wrapper.find("Header")).toHaveLength(1);
   });
 
-  it("renders a div with the class App-footer", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-footer")).toHaveLength(1);
+  it("App component contains Login component", () => {
+    expect(wrapper.find("Login")).toHaveLength(1);
   });
 
-  it("should not display CourseList when isLoggedIn is false", () => {
-    const { queryByText } = render(<App />);
-    const courseListElement = queryByText(/Course List/i);
-    expect(courseListElement).toBeNull();
+  it("App component contains Footer component", () => {
+    expect(wrapper.find("Footer")).toHaveLength(1);
   });
 
-  describe("when isLoggedIn is true", () => {
-    it("should not include the Login component", () => {
-      const { queryByText } = render(<App isLoggedIn={true} />);
-      const loginElement = queryByText(/Login/i);
-      expect(loginElement).toBeNull();
+  it("test to check that CourseList is not displayed inside App", () => {
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  });
+});
+
+describe("Testing <App isLoggedIn={true} />", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    wrapper = shallow(<App isLoggedIn={true} />);
+  });
+
+  it("the Login component is not included", () => {
+    expect(wrapper.find("Login")).toHaveLength(0);
+  });
+
+  it("the CourseList component is included", () => {
+    expect(wrapper.find("CourseList").exists());
+  });
+});
+
+describe("Testing <App logOut={function} />", () => {
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  it("verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
+    const wrapper = mount(
+      <App
+        logOut={() => {
+          console.log("ctrl and h are pressed");
+        }}
+      />
+    );
+    window.alert = jest.fn();
+    const inst = wrapper.instance();
+    const logout = jest.spyOn(inst, "logOut");
+    const alert = jest.spyOn(window, "alert");
+    const event = new KeyboardEvent("keydown", {
+      bubbles: true,
+      ctrlKey: true,
+      key: "h",
     });
-
-    it("should include the CourseList component", () => {
-      const { queryByText } = render(<App isLoggedIn={true} />);
-      const courseListElement = queryByText(/Course List/i);
-      expect(courseListElement).toBeInTheDocument();
-    });
+    document.dispatchEvent(event);
+    expect(alert).toBeCalledWith("Logging you out");
+    expect(logout).toBeCalled();
+    alert.mockRestore();
   });
 });
 
@@ -74,27 +106,96 @@ describe("Testing App Component's State />", () => {
   });
 });
 
-describe("Testing <App logOut={function} />", () => {
-  it("verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
-    const wrapper = mount(
-      <App
-        logOut={() => {
-          console.log("ctrl and h are pressed");
-        }}
-      />
-    );
-    window.alert = jest.fn();
-    const inst = wrapper.instance();
-    const logout = jest.spyOn(inst, "logOut");
-    const alert = jest.spyOn(window, "alert");
-    const event = new KeyboardEvent("keydown", {
-      bubbles: true,
-      ctrlKey: true,
-      key: "h",
-    });
-    document.dispatchEvent(event);
-    expect(alert).toBeCalledWith("Logging you out");
-    expect(logout).toBeCalled();
-    alert.mockRestore();
-  });
-});
+// describe("App component", () => {
+//   it("renders without crashing", () => {
+//     shallow(<App />);
+//   });
+
+//   it("renders a div with the class root-notifications", () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find(".root-notifications")).toHaveLength(1);
+//   });
+
+//   it("renders a div with the class App-header", () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find(".App-header")).toHaveLength(1);
+//   });
+
+//   it("renders a div with the class App-body", () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find(".App-body")).toHaveLength(1);
+//   });
+
+//   it("renders a div with the class App-footer", () => {
+//     const wrapper = shallow(<App />);
+//     expect(wrapper.find(".App-footer")).toHaveLength(1);
+//   });
+
+//   it("should not display CourseList when isLoggedIn is false", () => {
+//     const { queryByText } = render(<App />);
+//     const courseListElement = queryByText(/Course List/i);
+//     expect(courseListElement).toBeNull();
+//   });
+
+//   describe("when isLoggedIn is true", () => {
+//     it("should not include the Login component", () => {
+//       const { queryByText } = render(<App isLoggedIn={true} />);
+//       const loginElement = queryByText(/Login/i);
+//       expect(loginElement).toBeNull();
+//     });
+
+//     it("should include the CourseList component", () => {
+//       const { queryByText } = render(<App isLoggedIn={true} />);
+//       const courseListElement = queryByText(/Course List/i);
+//       expect(courseListElement).toBeInTheDocument();
+//     });
+//   });
+// });
+
+// describe("Testing App Component's State />", () => {
+//   let wrapper;
+
+//   beforeEach(() => {
+//     StyleSheetTestUtils.suppressStyleInjection();
+//     wrapper = shallow(<App />);
+//   });
+
+//   it("check if default value of displayDrawer in state is false", () => {
+//     expect(wrapper.state("displayDrawer")).toBe(false);
+//   });
+
+//   it("Verify that after calling handleDisplayDrawer, the state displayDrawer should now be true", () => {
+//     wrapper.instance().handleDisplayDrawer();
+//     expect(wrapper.state("displayDrawer")).toBe(true);
+//   });
+
+//   it("verify that after calling handleHideDrawer, the state displayDrawer is updated to be false", () => {
+//     wrapper.instance().handleHideDrawer();
+//     expect(wrapper.state("displayDrawer")).toBe(false);
+//   });
+// });
+
+// describe("Testing <App logOut={function} />", () => {
+//   it("verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out", () => {
+//     const wrapper = mount(
+//       <App
+//         logOut={() => {
+//           console.log("ctrl and h are pressed");
+//         }}
+//       />
+//     );
+//     window.alert = jest.fn();
+//     const inst = wrapper.instance();
+//     const logout = jest.spyOn(inst, "logOut");
+//     const alert = jest.spyOn(window, "alert");
+//     const event = new KeyboardEvent("keydown", {
+//       bubbles: true,
+//       ctrlKey: true,
+//       key: "h",
+//     });
+//     document.dispatchEvent(event);
+//     expect(alert).toBeCalledWith("Logging you out");
+//     expect(logout).toBeCalled();
+//     alert.mockRestore();
+//   });
+// });
--- 0x05-react_state/task_1/dashboard/src/Login/Login.js
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -1,27 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, css } from "aphrodite";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [enableSubmit, setEnableSubmit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  useEffect(() => {
+    if (email.length > 0 && password.length > 0) {
+      setEnableSubmit(true);
+    } else {
+      setEnableSubmit(false);
+    }
+  }, [email, password]);
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
+    setIsLoggedIn(true);
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+
+  
   return (
     <>
       <div className={css(styles.container)}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className={css(styles.input)}
-        ></input>
+        <form onSubmit={handleLoginSubmit}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChangeEmail}
+            value={email}
+            className={css(styles.input)}
+          ></input>
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className={css(styles.input)}
-        ></input>
-
-        <button className={css(styles.button)}>OK</button>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            onChange={handleChangePassword}
+            value={password}
+            name="password"
+            className={css(styles.input)}
+          ></input>
+          <input
+            type="submit"
+            disabled={!enableSubmit}
+            value="OK"
+            className={css(styles.button)}
+          ></input>
+        </form>
       </div>
     </>
   );
