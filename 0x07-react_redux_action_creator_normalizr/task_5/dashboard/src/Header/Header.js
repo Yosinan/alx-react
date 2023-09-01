57fff42d4652927a949517e47177b4d125c35048
Yoseph <ymymym4634@gmail.com>
2023-09-01 13:16:20 +0300

    React Redux action creator+normalizr

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/Header/Header.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "../assets/Holberton_logo.jpg";
+import { AppContext } from "../App/AppContext";
+import { StyleSheet, css } from "aphrodite";
+
+export default function Header() {
+  const { user, logOut } = React.useContext(AppContext);
+
+  return (
+    <>
+      <header className={css(styles.header)}>
+        <img src={logo} className={css(styles.logo)} alt="logo" />
+        <h1 className="title">School dashboard</h1>
+      </header>
+      {user.isLoggedIn && (
+        <section id="logoutSection">
+          <p>
+            Welcome<strong> {user.email} </strong>
+            <em>
+              <a href="#" onClick={logOut}>
+                (logout)
+              </a>
+            </em>
+          </p>
+        </section>
+      )}
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    color: "#e01d3f",
+    fontSize: "20px",
+    borderBottom: "3px solid #e01d3f",
+    width: "100%",
+  },
+  logo: {
+    width: "200px",
+  },
+});
